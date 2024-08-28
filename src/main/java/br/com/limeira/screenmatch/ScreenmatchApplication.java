2c0ce30ee43c356cd16c6dda43658ff9d2e493a9
RyanLimeira <limeira.ryan1110@gmail.com>
2024-08-28 08:01:46 -0300

    Inicio projeto spring

---
+++ src/main/java/br/com/limeira/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,26 @@
+package br.com.limeira.screenmatch;
+
+import br.com.limeira.screenmatch.model.DadosSerie;
+import br.com.limeira.screenmatch.service.ConsumoApi;
+import br.com.limeira.screenmatch.service.ConverteDados;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=stranger+things&apikey=43ad35c4");
+		System.out.println(json);
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
+	}
+}
