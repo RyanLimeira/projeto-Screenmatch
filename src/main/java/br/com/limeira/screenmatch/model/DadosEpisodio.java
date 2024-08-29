7829d36d6084c5a0b971908e61fcd85d8f57bea3
RyanLimeira <limeira.ryan1110@gmail.com>
2024-08-29 08:37:42 -0300

    desenvolvimento classe principal

---
+++ src/main/java/br/com/limeira/screenmatch/model/DadosEpisodio.java
@@ -0,0 +1,14 @@
+package br.com.limeira.screenmatch.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DadosEpisodio(@JsonAlias("Title") String titulo,
+                            @JsonAlias("Episode") int numero,
+                            @JsonAlias("imdbRating") String avaliacao,
+                            @JsonAlias("Released") String dataLancamento) {
+
+
+
+}
---
+++ src/main/java/br/com/limeira/screenmatch/principal/Principal.java
@@ -0,0 +1,47 @@
+package br.com.limeira.screenmatch.principal;
+
+import br.com.limeira.screenmatch.model.DadosEpisodio;
+import br.com.limeira.screenmatch.model.DadosSerie;
+import br.com.limeira.screenmatch.service.ConsumoApi;
+import br.com.limeira.screenmatch.service.ConverteDados;
+import br.com.limeira.screenmatch.model.DadosTemporada;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Principal {
+
+    private Scanner leitura = new Scanner(System.in);
+    private ConsumoApi consumo = new ConsumoApi();
+    private ConverteDados conversor = new ConverteDados();
+    private final String ENDERECO = "https://www.omdbapi.com/?t=";
+    private final String APIKEY = "&apikey=43ad35c4";
+
+    public void exibeMenu(){
+        System.out.println("Digite o nome da série para busca: ");
+        var nomeSerie = leitura.nextLine();
+        var json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + APIKEY);
+        DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+        System.out.println(dados);
+
+        List<DadosTemporada> temporadas = new ArrayList<>();
+
+		for (int i = 1; i <= dados.totalTemporadas(); i++){
+			json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + "&season=" + i + APIKEY);
+			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+			temporadas.add(dadosTemporada);
+		}
+
+		temporadas.forEach(System.out::println);
+
+//        for(int i = 0; i < dados.totalTemporadas(); i++){
+//            List<DadosEpisodio> episodiosTemporada = temporadas.get(i).episodios();
+//            for(int j = 0; j < episodiosTemporada.size(); j++){
+//                System.out.println(episodiosTemporada.get(j).titulo());
+//            }
+//        }
+
+        temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
+    }
+}
